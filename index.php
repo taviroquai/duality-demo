2b76aac255a67c6de38043e448f2c1f121b53663
Marco Afonso <mafonso333@gmail.com>
2014-10-21 17:26:15 +0100

    Remove unused

--- index.php
+++ index.php
@@ -9,14 +9,12 @@ use Duality\App;
 // Create application container
 $app = new App(dirname(__FILE__), $config);
 
-// Get server
+// Get server and request from globals
 $server = $app->call('server');
 $server->setRequest($server->getRequestFromGlobals($_SERVER, $_REQUEST));
 
 // Set demo routes
 $server->setHome('\Demo\Controller\Welcome@doIndex');
-$server->addRoute('/^\/json$/i',	'\Demo\Controller\Welcome@doUsersList');
-$server->addRoute('/^\/submit$/i',	'\Demo\Controller\Welcome@doSubmit');
 
 // Tell server to execute services
 $server->listen();
