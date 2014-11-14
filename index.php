d8fc2fe71bc3026efc1c3e55a2418595d317383b
Marco Afonso <mafonso333@gmail.com>
2014-11-14 16:49:39 +0000

    Update demo

--- index.php
+++ index.php
@@ -5,13 +5,22 @@ $config = include_once('./config/app.php');
 
 // What will we use in our application?
 use Duality\App;
+use Duality\Structure\Http\Request;
 
 // Create application container
 $app = new App(dirname(__FILE__), $config);
 
 // Get server and request from globals
 $server = $app->call('server');
-$server->setRequest($server->getRequestFromGlobals($_SERVER, $_REQUEST));
+$request = $server->getRequestFromGlobals($_SERVER, $_REQUEST);
+
+// Validate request. This is a Web application.
+if (!$request) {
+	die('HTTP request not found!' . PHP_EOL);
+}
+
+// Set server request
+$server->setRequest($request);
 
 // Set demo routes
 $server->setHome('\Demo\Controller\Welcome@doIndex');
